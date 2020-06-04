9641389898043c2b00e8e9534e3b28fb901c2e61
wong dawn <cqdsl18623169065@163.com>
2020-06-04 15:17:22 +0800

    upgrade head and tail

--- panads/pandas_learn3.py
+++ panads/pandas_learn3.py
@@ -1,9 +1,15 @@
 """Essential basic functionality"""
 import pandas as pd
 import numpy as np 
-index = pd.date_range('1/1/2000',periods=8)
-# print(index)
-s = pd.Series(np.random.randn(5),index=['a','b','c','d','e'])
-print(s)
-df =pd.DataFrame(np.random.randn(8,3),index=index,columns=['A','B','C'])
+# index = pd.date_range('1/1/2000',periods=8)
+# # print(index)
+# s = pd.Series(np.random.randn(5),index=['a','b','c','d','e'])
+# print(s)
+# df =pd.DataFrame(np.random.randn(8,3),index=index,columns=['A','B','C'])
 
+"""Head and tail"""
+# #To view a small sample of a series or dataframe object,use the "head()" and "tail()"method.The default number of 
+# #elements to display is five,but you may pass a custom number 
+long_series = pd.Series(np.random.randn(1000))
+print(long_series.head())
+print(long_series.tail())
